96f64c047685cf53f8e0aeb7f436b980c9eabaad
Angélica <angelica_shigematsu@outlook.com>
2024-01-07 21:05:56 -0300

    feat: added footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <TheHeader></TheHeader>
   <TheCapa></TheCapa>
+  <ThePromotionSection></ThePromotionSection>
+  <TheFooter></TheFooter>
 </template>
 
 <script setup>
 import './assets/base.css';
 import TheHeader from './components/TheHeader.vue'
 import TheCapa from './components/TheCapa.vue'
+import ThePromotionSection from './components/ThePromotionSection.vue'
+import TheFooter from './components/TheFooter.vue';
 
 </script>
 <style>
-
-
-
 </style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="footer-style">
+    <!-- <ul>
+      <li>Sobre</li>
+      <li>Promoções</li>
+    </ul> -->
+    <p class="description-footer">Desenvolvido por Angélica Shigematsu</p>
+    <p class="description-footer">2023 - 2024</p>
+  </div>
+</template>
+
+<style>
+.description-footer {
+  text-align: center;
+  display: flex;
+  justify-content: center;
+}
+.footer-style{
+  margin-top: 10vh;
+  background-color: gold;
+  height: 40vh;
+}
+</style>
